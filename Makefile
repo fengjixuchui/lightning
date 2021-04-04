981238354719459e290fa63841a0ca58a8402f26
youoralovedone <alexanderalecchang@gmail.com>
2021-04-04 02:29:41 -0700

    removed junk

--- Makefile
+++
@@ -1,9 +0,0 @@
-test:
-	g++ -o test1 test.cc -O4 -pthread -w
-	g++ -o test2 test2.cc -O4 -pthread -w
-	./test1
-	./test2
-
-why:
-	g++ -o programA programA.cc -O4 -pthread -w
-	./programA
--- redis_test.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect(("127.0.0.1", 6379))
-    s.sendall(b"set test_key test_value")
-    data = s.recv(2)
-
-print('Received', repr(data))
